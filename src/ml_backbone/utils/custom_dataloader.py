ff2e634d5c88ba3e8423b133492876c71bb6e802
Benjamin Mencer <bmencer@sdfiana005.sdf.slac.stanford.edu>
2024-06-21 14:01:04 -0700

    customs dataloader v2 with tests

--- src/ml_backbone/utils/custom_dataloader.py
+++ src/ml_backbone/utils/custom_dataloader.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 import torch
 class DataMilking(Dataset):
-    def __init__(self, root_dir = "", img_type="Ypdf", attributes = [], pulse_range = [], transform=None): #pulse_range is [min_pulses, max_pulses]
+    def __init__(self, root_dir = "", img_type="Ypdf", attributes = [], pulse_number = 2, transform=None): #pulse_range is [min_pulses, max_pulses]
         self.root_dir = root_dir
         self.transform = transform
         self.img_type = img_type
@@ -17,10 +17,13 @@ class DataMilking(Dataset):
         train_files = list(filter(lambda x: x.endswith('.h5'), train_files))
         
         for file in train_files:
+            # print("file: ", file)
             with h5py.File(os.path.join(root_dir, file), 'r') as f:
                 for shot in f.keys():
+                    # print("shot: ", shot)
+                    # print("attributes: ", list(f[shot].attrs.items()))
                     
-                    if pulse_range[0] < f[shot][img_type].attrs["npulses"] > pulse_range[1]: #only add shots where the npulses falls inside pulse_range
+                    if pulse_number == f[shot].attrs["npulses"]:
                         self.shot_paths.append([file, shot])
             
 
@@ -34,15 +37,17 @@ class DataMilking(Dataset):
         
         with h5py.File(file_path, 'r') as f:
             
-            img  = f[shot_id][self.img_type]
+            img  = f[shot_id][self.img_type][()]
             if self.transform:
                 img = self.transform(img)
             
             attribute_data = []
             for attribute in self.attributes:
-                attribute_data.append(f[shot_id][img_type].attrs[attribute])
+                attribute_data.append(f[shot_id].attrs[attribute])
         
-        return img, np.array(attribute_data)
+        # print("img: ", img)
+        # print("attribute_data", attribute_data)
+        return img, attribute_data
             
 
 transform = transforms.Compose([
