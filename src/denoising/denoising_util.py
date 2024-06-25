5d042ab88fb4f05cc282d058634a50cb46f4a10f
jhirschm <jhirschm@stanford.edu>
2024-06-25 15:02:10 -0700

    utils

--- src/denoising/denoising_util.py
+++ src/denoising/denoising_util.py
@@ -7,10 +7,8 @@ import json
 
 # External library imports
 import numpy as np
-import optuna
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset, random_split
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
@@ -23,7 +21,6 @@ from sklearn.metrics import (
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 import h5py
 import matplotlib.pyplot as plt
-from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 import copy
 import joblib
 from torch.nn import init
@@ -31,4 +28,3 @@ from torch.nn import init
 from sklearn.metrics import mean_squared_error
 
 import datetime
-import torch
