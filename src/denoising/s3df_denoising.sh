604868647173f56059c258618df539c811fb483d
jhirschm <jhirschm@stanford.edu>
2024-06-30 17:36:53 -0700

    updates

--- src/denoising/s3df_denoising.sh
+++ src/denoising/s3df_denoising.sh
@@ -12,15 +12,34 @@
 # source ~/.bashrc
 # source /sdf/group/lcls/ds/tools/conda_envs/jackh_pytorch/bin/activate cookie_ml
 
+
+# Check if the script argument is provided
+if [ -z "$1" ]; then
+    echo "No script specified. Usage: sbatch s3df_denoising.sh [training|fineTuning|evaluation]"
+    exit 1
+fi
+
 echo starting run 1 at: `date`
 # Check which GPU is being used
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 # Run the Python script with the specified arguments
 export PYTHONUNBUFFERED=1
 export PYTHONIOENCODING=utf-8
-# python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_training.py 
-# python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_training_fineTuning.py 
 
-python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_evaluation.py 
+case "$1" in
+    training)
+        python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_training.py
+        ;;
+    fineTuning)
+        python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_training_fineTuning.py
+        ;;
+    evaluation)
+        python3 /sdf/home/j/jhirschm/COOKIE_ML/src/denoising/ximg_to_ypdf_autoencoder_straight_evaluation.py
+        ;;
+    *)
+        echo "Invalid script specified. Usage: sbatch this_script.sh [training|fineTuning|evaluation]"
+        exit 1
+        ;;
+esac
 
 echo Finished at: `date`
