ff7b77168077d51880468d7c096a8036a84c85f0
jhirschm <jhirschm@stanford.edu>
2024-07-25 15:02:11 -0700

    updates

--- src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_training.py
+++ src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_training.py
@@ -100,7 +100,7 @@ def main():
     max_epochs = 200
     scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 10, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
     # model_save_dir = "/Users/jhirschm/Documents/MRCO/Data_Changed/Test"
-    model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/lstm_classifier/run_07062024_3/"
+    model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/lstm_classifier/run_07252024_noDenoising/"
     # Check if directory exists, otherwise create it
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
