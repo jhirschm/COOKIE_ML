9b3f0afe6a433b2c8f5989bb867c5a63d1a73b5f
jhirschm <jhirschm@stanford.edu>
2024-07-30 16:34:19 -0700

    updates

--- src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_training.py
+++ src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_training.py
@@ -79,7 +79,7 @@ def main():
     # Define the model
     # Create CustomLSTMClassifier model
     data = {
-        "hidden_size": 64,#128
+        "hidden_size": 128,
         "num_lstm_layers": 3,
         "bidirectional": True,
         "fc_layers": [32, 64],
@@ -113,7 +113,7 @@ def main():
     max_epochs = 200
     scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 10, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
     # model_save_dir = "/Users/jhirschm/Documents/MRCO/Data_Changed/Test"
-    model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/lstm_classifier/run_07302024_ypdf_0to1_test2/"
+    model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/lstm_classifier/run_07302024_ypdf_0to1_test3/"
     # Check if directory exists, otherwise create it
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
