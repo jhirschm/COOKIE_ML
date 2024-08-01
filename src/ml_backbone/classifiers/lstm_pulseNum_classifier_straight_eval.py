c6b2aa94b9e7a49fafda436b2afb0e1d9d695885
jhirschm <jhirschm@stanford.edu>
2024-07-31 22:27:29 -0700

    updates

--- src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_eval.py
+++ src/ml_backbone/classifiers/lstm_pulseNum_classifier_straight_eval.py
@@ -50,7 +50,7 @@ def main():
     # data_test = DataMilking_MilkCurds(root_dirs=[datapath_test], input_name="Ximg", pulse_handler=None, transform=None, pulse_threshold=4, test_batch=1, zero_to_one_rescale=False)
    
     # data_test = DataMilking_MilkCurds(root_dirs=[datapath_test], input_name="Ximg", pulse_handler=None, transform=None, pulse_threshold=4, zero_to_one_rescale=False, test_batch=1)
-    data_test = DataMilking_MilkCurds(root_dirs=[datapath_test], input_name="Ximg", pulse_handler=None, transform=None, pulse_threshold=5, zero_to_one_rescale=False, phases_labeled=True, phases_labeled_max=2)
+    data_test = DataMilking_MilkCurds(root_dirs=[datapath_test], input_name="Ximg", pulse_handler=None, transform=None, pulse_threshold=5, zero_to_one_rescale=False, phases_labeled=False, phases_labeled_max=2)
 
 
 
