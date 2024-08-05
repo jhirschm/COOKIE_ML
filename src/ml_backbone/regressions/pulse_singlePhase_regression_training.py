e7f13a2f11d95475c6032424dc7324ca845da9c8
jhirschm <jhirschm@stanford.edu>
2024-08-05 14:20:26 -0700

    fix

--- src/ml_backbone/regressions/pulse_singlePhase_regression_training.py
+++ src/ml_backbone/regressions/pulse_singlePhase_regression_training.py
@@ -272,7 +272,7 @@ def main():
     regression_model.to(device)
     # Define the loss function and optimizer
     criterion = nn.MSELoss(reduction='none')
-    optimizer = torch.optim.Adam(regression_model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(regression_model.parameters(), lr=0.0001)
     max_epochs = 200
     scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 10, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
 
