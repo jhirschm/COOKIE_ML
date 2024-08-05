06288ff9783dbf0516076abcb2589da4e9044a08
jhirschm <jhirschm@stanford.edu>
2024-08-05 14:17:55 -0700

    fix

--- src/ml_backbone/regressions/pulse_singlePhase_regression_training.py
+++ src/ml_backbone/regressions/pulse_singlePhase_regression_training.py
@@ -247,17 +247,17 @@ def main():
 
     #Trying LSTM 
     fc_layers = [
-    [nn.Linear(128, 256), nn.ReLU()],
-    [nn.Linear(256, 64), nn.ReLU()],
-    [nn.Linear(64, 32), nn.ReLU()],
+    [nn.Linear(64, 128), nn.ReLU()],
+    [nn.Linear(128, 32), nn.ReLU()],
     [nn.Linear(32, 8), nn.ReLU()],
+    # [nn.Linear(32, 8), nn.ReLU()],
     [nn.Linear(8, 1), nn.ReLU()]  
     ]
 
     # Define LSTM configuration
     lstm_config = {
         'input_size': 512,  # Example input size for LSTM
-        'hidden_size': 64,
+        'hidden_size': 32,
         'num_layers': 2,
         'bidirectional': True
     }
@@ -269,9 +269,10 @@ def main():
                             dtype=torch.float32, 
                             use_dropout=False, 
                             dropout_rate=0.1)
+    regression_model.to(device)
     # Define the loss function and optimizer
     criterion = nn.MSELoss(reduction='none')
-    optimizer = torch.optim.Adam(classModel.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(regression_model.parameters(), lr=0.001)
     max_epochs = 200
     scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 10, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
 
