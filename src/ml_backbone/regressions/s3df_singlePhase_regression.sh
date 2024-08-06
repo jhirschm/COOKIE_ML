5bad2d6916a95da227db91da943b3390f3c30b59
jhirschm <jhirschm@stanford.edu>
2024-08-06 12:47:54 -0700

    fix

--- src/ml_backbone/regressions/s3df_singlePhase_regression.sh
+++ src/ml_backbone/regressions/s3df_singlePhase_regression.sh
@@ -32,7 +32,7 @@ case "$1" in
         ;;
     
     evaluation)
-        python3 /sdf/home/j/jhirschm/COOKIE_ML/src/ml_backbone/regressions/pulse_singlePhase_regression_eval.py
+        python3 /sdf/home/j/jhirschm/COOKIE_ML/src/ml_backbone/regressions/pulse_singlePhase_regression_evaluation.py
         ;;
     *)
         echo "Invalid script specified. Usage: sbatch this_script.sh [training|evaluation]"
