1f83a77c7455c534835aa29c611c9009af636e1b
jhirschm <jhirschm@stanford.edu>
2024-08-15 20:55:23 -0700

    fix

--- src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
+++ src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
@@ -478,7 +478,7 @@ def main():
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     max_epochs = 200
     scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 10, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
 
