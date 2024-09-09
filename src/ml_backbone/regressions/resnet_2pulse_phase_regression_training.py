5f19e00a61c85b90ee3a1028e04d914057450a52
jhirschm <jhirschm@stanford.edu>
2024-09-08 22:52:02 -0700

    loss

--- src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
+++ src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
@@ -736,6 +736,11 @@ def train_model(model, train_dataloader, val_dataloader, criterion, optimizer, s
                     print(f"Early stopping at epoch {epoch+1}\n")
                     f.write(f"Early stopping at epoch {epoch+1}\n")
                     break
+                if not f.closed:
+                    f.write(f"Early stopping at epoch {epoch+1}\n")
+                    f.flush()  # Flush the buffer to write to the file
+                else:
+                    print("Error: File is already closed, cannot write.")
                 f.flush() # Flush the buffer to write to the file
         # Save the output to the specified file
         run_summary_path = f"{model_save_dir}/{identifier}"+ "_run_summary.txt"
