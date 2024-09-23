7f6367762fb0c2f6d5089c71339a77579ac8a0ba
jhirschm <jhirschm@stanford.edu>
2024-09-23 11:57:49 -0700

    hope

--- src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
+++ src/ml_backbone/regressions/resnet_2pulse_phase_regression_training.py
@@ -1049,8 +1049,8 @@ def main():
     num_classes = 4000#1024
     # model = resnet152(num_classes=num_classes)
     # model = resnet34(num_classes=num_classes)
-    model = resnet50(num_classes=num_classes)
-    # model = resnet34(num_classes=num_classes)
+    # model = resnet50(num_classes=num_classes)
+    model = resnet34(num_classes=num_classes)
     # model = resnet18(num_classes=num_classes)
 
 
@@ -1072,11 +1072,11 @@ def main():
     # Load Dataset and Feed to Dataloader
     # datapath_train = "/sdf/data/lcls/ds/prj/prjs2e21/results/2-Pulse_04232024/Processed_07312024_0to1/train/"
     datapath_train = "/sdf/scratch/lcls/ds/prj/prjs2e21/scratch/fast_data_access/2-Pulse_04232024/Processed_07312024_0to1/train/"
-
+    datapath_train = "/sdf/scratch/lcls/ds/prj/prjs2e21/scratch/fast_data_access/2-Pulse_04232024/Processed_07312024_0to1/train/assorted"
     pulse_specification = None
 
 
-    data_train = DataMilking_MilkCurds(root_dirs=[datapath_train], input_name="Ximg", pulse_handler=None, transform=None, test_batch = 5, pulse_threshold=4, zero_to_one_rescale=False, phases_labeled=True, phases_labeled_max=2, inverse_radon=False)
+    data_train = DataMilking_MilkCurds(root_dirs=[datapath_train], input_name="Ximg", pulse_handler=None, transform=None, pulse_threshold=4, zero_to_one_rescale=False, phases_labeled=True, phases_labeled_max=2, inverse_radon=False)
 
     # data_val = DataMilking_MilkCurds(root_dirs=[datapath_val], input_name="Ypdf", pulse_handler=None, transform=None, pulse_threshold=4, test_batch=3)
 
@@ -1111,6 +1111,7 @@ def main():
     model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/regression/run_09122024_Resnext152_dif_Ximg_Denoised_1_newScheduler"
     model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/regression/run_09122024_Resnext50_dif_Ximg_Denoised_1_newScheduler"
 
+    model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/regression/run_09232024_Resnet34_dif_Ximg_Denoised_assorted"
 
     # model_save_dir = "/sdf/data/lcls/ds/prj/prjs2e21/results/COOKIE_ML_Output/regression/run_08302024_Resnext34_dif_Ypdf_1"
 
@@ -1127,7 +1128,7 @@ def main():
     max_epochs = 200
     # scheduler = CustomScheduler(optimizer, patience=3, early_stop_patience = 8, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
     scheduler = CustomSchedulerWeightUpdate(optimizer = optimizer, model=model, patience=3, early_stop_patience = 8, cooldown=2, lr_reduction_factor=0.5, max_num_epochs = max_epochs, improvement_percentage=0.001)
-    identifier = "Resnext50_dif_XimgDenoised_wrapping"
+    identifier = "Resnext34_dif_XimgDenoised_wrapping_assorted"
 
     '''
     denoising
