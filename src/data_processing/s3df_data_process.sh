e1e71a019f735cd4c58ce35afa7f6e9cdadca007
jhirschm <jhirschm@stanford.edu>
2024-10-09 13:45:24 -0700

    hope

--- src/data_processing/s3df_data_process.sh
+++ src/data_processing/s3df_data_process.sh
@@ -14,13 +14,15 @@ export PYTHONUNBUFFERED=1
 export PYTHONIOENCODING=utf-8
 # Define paths to your data files and scalers
 FILE_PATHS="/sdf/data/lcls/ds/prj/prjs2e21/results/2-Pulse_04232024/"
+FILE_PATHS="/sdf/data/lcls/ds/prj/prjs2e21/results/pois-dist_Pulses_03302024/"
 SCALER_NAME="min_max_scaler"
 SAVEPATH="/sdf/data/lcls/ds/prj/prjs2e21/results/2-Pulse_04232024//Processed_07312024_0to1"
+SAVEPATH="/sdf/data/lcls/ds/prj/prjs2e21/results/pois-dist_Pulses_03302024//Processed_10092024_0to1"
 ENERGY_ELEMENTS=512
 TEST_MODE=False
 
-TRAIN=0.9
-TEST=0.1
+TRAIN=0.0
+TEST=1.0
 
 # Run the Python script with the specified arguments
 python3 /sdf/home/j/jhirschm/COOKIE_ML/src/data_processing/universal_cookiesimslim_processor.py $FILE_PATHS --scaler_name $SCALER_NAME  --savepath $SAVEPATH --energy_elements $ENERGY_ELEMENTS --train_test_split $TRAIN $TEST 
