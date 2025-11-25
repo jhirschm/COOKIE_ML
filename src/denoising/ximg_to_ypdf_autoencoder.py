86ded4c3af746eeb7cb3a7e6f85d4593ceaf48c7
Benjamin Mencer <bmencer@slac.stanford.edu>
2025-11-24 22:08:23 -0800

    result saving cleanup

--- src/denoising/ximg_to_ypdf_autoencoder.py
+++ src/denoising/ximg_to_ypdf_autoencoder.py
@@ -604,7 +604,9 @@ class Ximg_to_Ypdf_Autoencoder(nn.Module):
                     inputs_np = inputs.cpu().numpy()
                     outputs_np = outputs.cpu().numpy()
                     labels_np = labels.cpu().numpy()
-                    results[i] = (inputs_np, outputs_np, labels_np, loss.item())
+                    # results[i] = (inputs_np, outputs_np, labels_np, loss.item())
+                    results[i] = (inputs_np, outputs_np, labels_np)
+                    
                 i+=1
 
         avg_loss = running_loss / len(dataloader)
