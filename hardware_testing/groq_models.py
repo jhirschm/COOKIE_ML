3159454ea76081f166a8b214a64624d481f280b5
Peter Rakyta <peter.rakyta@tk.elte.hu>
2026-02-02 09:51:47 +0000

    move function load model state into a separate file

--- hardware_testing/groq_models.py
+++ hardware_testing/groq_models.py
@@ -23,6 +23,7 @@ from compile_lpu_model import (
     compile_pulsenum_classifier_workflow_with_ttl,
     CompilerType,
 )
+from load_model_config import get_model_config
 import groq.api as g
 
 
@@ -74,69 +75,6 @@ def get_activation_function(activation_function_name):
         return nn.Tanh()
     else:
         return None
-
-
-def get_model_config(config_path=os.path.join(current_dir, "model_config.json")):
-
-    with open(config_path, "r") as f:
-        config = json.load(f)
-
-    input_size = config["input_size"]
-    batch_num = config["batch_num"]
-    encoder_config = config["encoder"]
-    decoder_config = config["decoder"]
-    zero_mask_classifier_config = config["zero_mask_classifier"]
-    lstm_pulseNum_classifier_config = config["lstm_pulseNum_classifier"]
-    layer_configurations_encoder = []
-    layer_configurations_decoder = []
-    conv_layer_configurations_zero_mask_classifier = []
-    fc_layer_configurations_zero_mask_classifier = []
-    lstm_layer_configurations_lstm_pulseNum_classifier = (
-        lstm_pulseNum_classifier_config["lstm_layer_configurations"][0]
-    )
-    fc_layer_configurations_lstm_pulseNum_classifier = lstm_pulseNum_classifier_config[
-        "fc_layer_configurations"
-    ][0]
-
-    # Add batch_num to each layer configuration
-    for layer_conf in encoder_config["layer_configurations"]:
-        layer_conf_with_batch = layer_conf.copy()
-        layer_conf_with_batch["batch_num"] = batch_num
-        layer_configurations_encoder.append(layer_conf_with_batch)
-
-    for layer_conf in decoder_config["layer_configurations"]:
-        layer_conf_with_batch = layer_conf.copy()
-        layer_conf_with_batch["batch_num"] = batch_num
-        layer_configurations_decoder.append(layer_conf_with_batch)
-
-    for layer_conf in zero_mask_classifier_config["conv_layer_configurations"]:
-        layer_conf_with_batch = layer_conf.copy()
-        layer_conf_with_batch["batch_num"] = batch_num
-        conv_layer_configurations_zero_mask_classifier.append(layer_conf_with_batch)
-
-    for layer_conf in zero_mask_classifier_config["fc_layer_configurations"]:
-        layer_conf_with_batch = layer_conf.copy()
-        layer_conf_with_batch["batch_num"] = batch_num
-        fc_layer_configurations_zero_mask_classifier.append(layer_conf_with_batch)
-
-    lstm_layer_configurations_lstm_pulseNum_classifier["batch_num"] = batch_num
-    lstm_layer_configurations_lstm_pulseNum_classifier["layer_norm"] = (
-        lstm_pulseNum_classifier_config.get("layer_norm", False)
-    )
-    fc_layer_configurations_lstm_pulseNum_classifier["batch_num"] = batch_num
-    fc_layer_configurations_lstm_pulseNum_classifier["layer_norm"] = (
-        lstm_pulseNum_classifier_config.get("layer_norm", False)
-    )
-
-    return (
-        layer_configurations_encoder,
-        layer_configurations_decoder,
-        conv_layer_configurations_zero_mask_classifier,
-        fc_layer_configurations_zero_mask_classifier,
-        lstm_layer_configurations_lstm_pulseNum_classifier,
-        fc_layer_configurations_lstm_pulseNum_classifier,
-        input_size,
-    )
 
 
 def main():
@@ -389,7 +327,7 @@ def main():
     print("=" * 25)
 
     print(
-        f"Groq runner total execution time: {elapsed_time:.6f} seconds ({elapsed_time * 1000000:.3f} microseconds)"
+        f"Groq runner total execution time per iteration: {elapsed_time:.6f} seconds ({elapsed_time * 1000000:.3f} microseconds)"
     )
 
     results_groq = runner.invoke(inputs)
